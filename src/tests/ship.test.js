ef2e29e420709836b8e433493004a244b474983a
Aminu Mukhtar Mado <madoaminu@gmail.com>
2021-12-31 09:41:12 +0100

    added the isHit and isSunk methods

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -9,4 +9,30 @@ describe("Ship", () => {
   test("defines coordinate()", () => {
     expect(typeof destroyer.coordinate).toBe("function");
   });
+  test("case 1 right coordinate([0,1],5,'right')", () => {
+    expect(destroyer.coordinate([0, 1], 5, "right")).toStrictEqual([
+      [0, 1],
+      [0, 2],
+      [0, 3],
+      [0, 4],
+      [0, 5],
+    ]);
+  });
+  test("case 2 down coordinate([0,1],5,'down')", () => {
+    expect(destroyer.coordinate([0, 1], 5, "down")).toStrictEqual([
+      [0, 1],
+      [1, 1],
+      [2, 1],
+      [3, 1],
+      [4, 1],
+    ]);
+  });
+  test("case 1 isHit([0,1]) of a test body", () => {
+    expect(destroyer.isHit([0, 1])).toBe(
+      destroyer.health == [true, false, false, false, false]
+    );
+  });
+  test("case 1 isSunk()", () => {
+    expect(destroyer.isSunk()).toBe(true);
+  });
 });
