162baa39a0a1277f03fff350556fc053c6bcf9fa
Aminu Mukhtar Mado <madoaminu@gmail.com>
2021-12-31 09:40:42 +0100

    added the isSunk and isHit methods

--- src/ship.js
+++ src/ship.js
@@ -1,13 +1,12 @@
 class Ship {
-  constructor(head, direction, length) {
+  constructor(head, length, direction) {
     this.head = head;
-    this.direction = direction;
     this.length = length;
+    this.direction = direction;
+    this.health = new Array(this.length).fill(false);
   }
-  body = [];
-  health = new Array(this.length).fill(false);
+  randomshit = 22;
   //to find the index of an item you can use .indexOf() in javascript
-
   coordinate(head, length, direction) {
     //direction could be right and down only
     //right
@@ -18,8 +17,48 @@ class Ship {
     // first element remains constant
     // second element changes by an increment of 1
     //
-    for (let i = 0; i < length; i++) {
-      body.push([head[0], head[1] + i]);
+    let body = [];
+
+    switch (direction) {
+      case "right":
+        for (let i = 0; i < length; i++) {
+          body.push([head[0], head[1] + i]);
+        }
+        break;
+      case "down":
+        for (let i = 0; i < length; i++) {
+          body.push([head[0] + i, head[1]]);
+        }
+        break;
+      default:
+        return;
+    }
+    return body;
+  }
+  isHit() {
+    // check if the coordinate is equal to a point in the body
+    // if yes get the index of the part that was hit
+    // change that part of the index to true
+    // This just changes the health array
+    let testBody = [
+      [0, 1],
+      [0, 2],
+      [0, 3],
+      [0, 4],
+      [0, 5],
+    ];
+    let idx = testBody.indexOf([0, 1]);
+    this.health[idx] = true;
+  }
+  isSunk() {
+    //this returns a true or false
+    // by default it's a false
+    // it's only true when the health array is all true
+    // loop through each element of the health array and if they are all true return true else return false
+    for (let i = 0; i < this.health.length; i++) {
+      if (this.health[i] == false) {
+        return false;
+      } else return true;
     }
   }
 }
