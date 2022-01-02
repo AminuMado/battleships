7e5f3a848fe082a409e0014905593b087c8c6897
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-01-02 22:03:00 +0100

    removed the getShip method and added the placeShip method

--- src/gameboard.js
+++ src/gameboard.js
@@ -9,49 +9,26 @@ import createShip from "./ship.js";
 // Gameboards should keep track of missed attacks so they can display them properly.
 // Gameboards should be able to report whether or not all of their ships have been sunk.
 function createGameBoard(width, height) {
-  let gameboard = [];
-  let ships = {};
+  let gameBoard = [];
+  let ships = [];
   buildBoard();
+
   /// for example
   //   board [0 0 0 0 0
   //          1 s1 0 0
   //          0  0 s2 s2
   //          ]
   function buildBoard() {
-    gameboard = new Array(width * height).fill(null);
+    gameBoard = new Array(width * height).fill(null);
   }
-  function getShips() {
-    // you get the ships from the ship factory
-    // save their coordinates in a ships array or object
-    // these ships are an aircraft carrier, battleship, cruiser, submarine, and destroyer
-    const aircraft = createShip(10, 5, "right").getBody();
-    const battleship = createShip(22, 4, "down").getBody();
-    const cruiser = createShip(90, 3, "right").getBody();
-    const submarine = createShip(77, 3, "down").getBody();
-    const destroyer = createShip(66, 2, "right").getBody();
-    ships = { aircraft, battleship, cruiser, submarine, destroyer };
-    return ships;
-    // // { aircraft: [10,11,12,13,14];
-    //      battleship: [22,32,42,52];
-    //         cruiser: [90,91,92];
-    //         submarine:[77,87,97];
-    //         destroyer: [66,68]
-    //     }
-    // or a ship is just [[10,11,12,13,14],[22,32,42,52],[90,91,92],[77,87,97],[66,68]]
-  }
-  function placeShips() {
-    //all you do is place the ships coordinate on the gameboard
-    // ships should be denoted with a ship name
-    // get the position of the ship from its body
-    // place the postion of the ship on the gameboard
-    // eg destroyer.getBody() = [3,4,5,6,7]
-    // gameboard[3] = destroyer
-    // gameboard[4] = destroyer
-    // gameboard[5] = destroyer
-    // gameboard[6] = destroyer
-    // gameboard[7] = destroyer
-    // take the value of the ship in the array
-    // use the value as the index and place the ship name there
+
+  function placeShip(head, length, direction) {
+    const ship = createShip(head, length, direction);
+    const shipName = ship.getName();
+    const shipCoordinate = ship.getBody();
+    shipCoordinate.forEach((element) => {
+      gameBoard[element] = shipName;
+    });
   }
   function receiveAttack(coordinate) {
     //if gameboard[coordinate] != null{
@@ -74,7 +51,7 @@ function createGameBoard(width, height) {
     // the function call returns a true or a false and if
     // they are all true then all ships are sunk
   }
-  return { gameboard, receiveAttack, placeShips, getShips };
+  return { gameBoard, ships, receiveAttack, placeShip, getShips };
 }
 
 module.exports = createGameBoard;
