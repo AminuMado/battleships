e9bfaeb02bc6d48f96881f69db3d0298527751f6
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-01-02 05:47:14 +0100

    refactored to test the new createship factory funtion

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,7 +1,7 @@
 const createShip = require("../ship.js");
 
 describe("Ship", () => {
-  const destroyer = createShip([0, 0], 5, "right");
+  const destroyer = createShip(0, 5, "right");
 
   test("defines Destroyer ship", () => {
     expect(typeof destroyer).toBe("object");
@@ -10,33 +10,28 @@ describe("Ship", () => {
     expect(typeof destroyer.getBody).toBe("function");
   });
   test("case 1 getBody()", () => {
-    expect(destroyer.getBody()).toStrictEqual([
-      [0, 0],
-      [0, 1],
-      [0, 2],
-      [0, 3],
-      [0, 4],
-    ]);
+    expect(destroyer.getBody()).toStrictEqual([0, 1, 2, 3, 4]);
   });
 
-  test("case 1 isHit([0,4])", () => {
-    expect(destroyer.isHit([0, 3])).toStrictEqual([
+  test("case 1 isHit(4)", () => {
+    destroyer.isHit(4);
+    expect(destroyer.getHealth()).toStrictEqual([
+      false,
       false,
       false,
       false,
       true,
-      false,
     ]);
   });
   test("case 1 isSunk()", () => {
     expect(destroyer.isSunk()).toBe(false);
   });
   test("case 2 isSunk()", () => {
-    destroyer.isHit([0, 0]);
-    destroyer.isHit([0, 1]);
-    destroyer.isHit([0, 2]);
-    destroyer.isHit([0, 3]);
-    destroyer.isHit([0, 4]);
+    destroyer.isHit(0);
+    destroyer.isHit(1);
+    destroyer.isHit(2);
+    destroyer.isHit(3);
+    destroyer.isHit(4);
     expect(destroyer.isSunk()).toBe(true);
   });
 });
