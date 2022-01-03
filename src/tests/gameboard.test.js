8d6860475d0b829278d4ccb9a8186cfe63395808
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-01-03 16:28:44 +0100

    added tests for a reveive attack function

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -14,21 +14,41 @@ describe("gameBoard", () => {
     myBoard.placeShip(22, 4, "vertical");
     myBoard.placeShip(90, 3, "horizontal");
     myBoard.placeShip(66, 2, "vertical");
-    expect(myBoard.gameBoard[10]).toStrictEqual("aircraft");
-    expect(myBoard.gameBoard[12]).toStrictEqual("aircraft");
-    expect(myBoard.gameBoard[14]).toStrictEqual("aircraft");
-    expect(myBoard.gameBoard[22]).toStrictEqual("battleship");
-    expect(myBoard.gameBoard[52]).toStrictEqual("battleship");
-    expect(myBoard.gameBoard[90]).toStrictEqual("submarine");
-    expect(myBoard.gameBoard[92]).toStrictEqual("submarine");
-    expect(myBoard.gameBoard[66]).toStrictEqual("destroyer");
-    expect(myBoard.gameBoard[76]).toStrictEqual("destroyer");
-    expect(myBoard.gameBoard[99]).toStrictEqual(null);
-    //   aircraft: [10, 11, 12, 13, 14],
-    //   battleship: [22, 32, 42, 52],
-    //   cruiser: [90, 91, 92],
-    //   submarine: [77, 87, 97],
-    //   destroyer: [66, 67],
-    // });
+    expect(myBoard.getGameBoard()[10]).toStrictEqual("aircraft");
+    expect(myBoard.getGameBoard()[12]).toStrictEqual("aircraft");
+    expect(myBoard.getGameBoard()[14]).toStrictEqual("aircraft");
+    expect(myBoard.getGameBoard()[22]).toStrictEqual("battleship");
+    expect(myBoard.getGameBoard()[52]).toStrictEqual("battleship");
+    expect(myBoard.getGameBoard()[90]).toStrictEqual("submarine");
+    expect(myBoard.getGameBoard()[92]).toStrictEqual("submarine");
+    expect(myBoard.getGameBoard()[66]).toStrictEqual("destroyer");
+    expect(myBoard.getGameBoard()[76]).toStrictEqual("destroyer");
+    expect(myBoard.getGameBoard()[99]).toStrictEqual(null);
   });
+  test("defines receiveAttack()", () => {
+    expect(typeof myBoard.receiveAttack).toBe("function");
+  });
+  test("case 1, hit receiveAttack(50)", () => {
+    myBoard.receiveAttack(10);
+    myBoard.receiveAttack(11);
+    myBoard.receiveAttack(12);
+    myBoard.receiveAttack(13);
+    myBoard.receiveAttack(14);
+    expect(myBoard.getGameBoard()[10]).toBe(1);
+    //also change the health of the coordinate on the ship that was hit
+    // for example lets say a destroyer was at [50,51]
+    // destroyer health [false,false] initially
+    // after reveiveAttack(50) expect destroyer.health [true,false]
+    // ships[0].getHealth() to be [true,false]
+    expect(myBoard.getShips()[0].getHealth()).toStrictEqual([
+      true,
+      true,
+      false,
+      true,
+      true,
+    ]);
+  });
+  // test("case 2, miss receiveAttack(50)", () => {
+  //   expect(myBoard.getGameBoard()[99]).toBe(0);
+  // });
 });
