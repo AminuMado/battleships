4b302aee2c134f370b2dc6480a6c1da87725a6ed
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-01-04 15:08:14 +0100

    created the player factory with  a gameboard attached to it

---
+++ src/player.js
@@ -0,0 +1,18 @@
+import createGameBoard from "./gameboard.js";
+
+function createPlayer(name, type, width, height) {
+  let gameBoard = createGameBoard(width, height);
+
+  function getName() {
+    return name;
+  }
+  function getType() {
+    return type;
+  }
+  function getGameBoard() {
+    return gameBoard;
+  }
+  return { getName, getType, getGameBoard };
+}
+
+module.exports = createPlayer;
