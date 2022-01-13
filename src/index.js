96c4593b61e1c85513a7cda5b100d63d95a0305c
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-01-13 14:39:21 +0100

    refactored the code

--- src/index.js
+++ src/index.js
@@ -1,273 +1,27 @@
 import "./style.css";
-// import carrierSrc from "../src/assets/images/carrier.svg";
-// import destroyerSrc from "../src/assets/images/destroyer.svg";
-// import patrolSrc from "../src/assets/images/patrol.svg";
-// import submarineSrc from "../src/assets/images/submarine.svg";
-
-// const shipContainer = document.querySelector(".ship-container");
-// function addShipsImg() {
-//   const div1 = document.createElement("div");
-//   const div2 = document.createElement("div");
-//   const div3 = document.createElement("div");
-//   const div4 = document.createElement("div");
-//   const carrierImg = document.createElement("img");
-//   const destroyerImg = document.createElement("img");
-//   const patrolImg = document.createElement("img");
-//   const submarineImg = document.createElement("img");
-//   carrierImg.src = carrierSrc;
-//   destroyerImg.src = destroyerSrc;
-//   patrolImg.src = patrolSrc;
-//   submarineImg.src = submarineSrc;
-//   div1.classList.add("carrier");
-//   // div1.appendChild(carrierImg);
-//   // div2.appendChild(destroyerImg);
-//   // div3.appendChild(patrolImg);
-//   // div4.appendChild(submarineImg);
-//   shipContainer.appendChild(div1);
-//   shipContainer.appendChild(div2);
-//   shipContainer.appendChild(div3);
-//   shipContainer.appendChild(div4);
-// }
-// // addShipsImg();
-// const shipsPlacedBoard = document.querySelector(".ships-placed-board");
-// const boardContainer = document.querySelector(".board-container");
-// const shipsContainer = document.querySelector(".ships-container");
-// let selectedShip = null;
-// let type = null;
-// const cells = document.querySelectorAll(".grid-cell");
-
-// shipsContainer.addEventListener("click", (e) => {
-//   let ship = e.target.classList;
-
-//   if (ship.contains("ship")) {
-//     selectedShip = e.target;
-//     type = e.target.classList[0];
-//     console.log(selectedShip);
-//   }
-// });
-// cells.forEach((cell) => {
-//   cell.addEventListener("click", (e) => {
-//     let spot = e.target.dataset.id;
-
-//     console.log(type);
-//     if (selectedShip != null) {
-//       console.log(spot[0]);
-//       let placedShip = createShipDom(spot, type);
-//       shipsPlacedBoard.appendChild(placedShip);
-//     }
-//   });
-// });
-
-// function createShipDom(name, head, length, direction) {
-//   const div = document.createElement("div");
-//   const idx = head.toString();
-//   if (direction === "horizontal") {
-//     switch (name) {
-//       case "carrier":
-//         div.classList.add("carrier");
-//         div.style.gridRowStart = `${parseInt(idx[0]) + 1}`;
-//         div.style.gridRowEnd = `span 1`;
-//         div.style.gridColumnStart = `${parseInt(idx[1]) + 1}`;
-//         div.style.gridColumnEnd = `span ${length}`;
-//         break;
-//       case "battleship":
-//         div.classList.add("battleship");
-//         div.style.gridRowStart = `${parseInt(idx[0]) + 1}`;
-//         div.style.gridRowEnd = `span 1`;
-//         div.style.gridColumnStart = `${parseInt(idx[1]) + 1}`;
-//         div.style.gridColumnEnd = `span ${length}`;
-//         break;
-//       case "destroyer":
-//         div.classList.add("destroyer");
-//         div.style.gridRowStart = `${parseInt(idx[0]) + 1}`;
-//         div.style.gridRowEnd = `span 1`;
-//         div.style.gridColumnStart = `${parseInt(idx[1]) + 1}`;
-//         div.style.gridColumnEnd = `span ${length}`;
-//         break;
-//       case "cruiser":
-//         div.classList.add("cruiser");
-//         div.style.gridRowStart = `${parseInt(idx[0]) + 1}`;
-//         div.style.gridRowEnd = `span 1`;
-//         div.style.gridColumnStart = `${parseInt(idx[1]) + 1}`;
-//         div.style.gridColumnEnd = `span ${length}`;
-//         break;
-//       case "submarine":
-//         div.classList.add("submarine");
-//         div.style.gridRowStart = `${parseInt(idx[0]) + 1}`;
-//         div.style.gridRowEnd = `span 1`;
-//         div.style.gridColumnStart = `${parseInt(idx[1]) + 1}`;
-//         div.style.gridColumnEnd = `span ${length}`;
-//         break;
-//       case "patrol":
-//         div.classList.add("patrol");
-//         div.style.gridRowStart = `${parseInt(idx[0]) + 1}`;
-//         div.style.gridRowEnd = `span 1`;
-//         div.style.gridColumnStart = `${parseInt(idx[1]) + 1}`;
-//         div.style.gridColumnEnd = `span ${length}`;
-//         break;
-//       default:
-//         return;
-//     }
-//   } else if (direction === "vertical") {
-//     switch (name) {
-//       case "carrier":
-//         div.classList.add("carrier");
-//         div.style.gridRowStart = `${parseInt(idx[0]) + 1}`;
-//         div.style.gridRowEnd = `span 1`;
-//         div.style.gridColumnStart = `${parseInt(idx[1]) + 1}`;
-//         div.style.gridColumnEnd = `span ${length}`;
-//         break;
-//       case "battleship":
-//         div.classList.add("battleship");
-//         div.style.gridRowStart = `${parseInt(idx[0]) + 1}`;
-//         div.style.gridRowEnd = `span 1`;
-//         div.style.gridColumnStart = `${parseInt(idx[1]) + 1}`;
-//         div.style.gridColumnEnd = `span ${length}`;
-//         break;
-//       case "destroyer":
-//         div.classList.add("destroyer");
-//         div.style.gridRowStart = `${parseInt(idx[0]) + 1}`;
-//         div.style.gridRowEnd = `span 1`;
-//         div.style.gridColumnStart = `${parseInt(idx[1]) + 1}`;
-//         div.style.gridColumnEnd = `span ${length}`;
-//         break;
-//       case "cruiser":
-//         div.classList.add("cruiser");
-//         div.style.gridRowStart = `${parseInt(idx[0]) + 1}`;
-//         div.style.gridRowEnd = `span 1`;
-//         div.style.gridColumnStart = `${parseInt(idx[1]) + 1}`;
-//         div.style.gridColumnEnd = `span ${length}`;
-//         break;
-//       case "submarine":
-//         div.classList.add("submarine");
-//         div.style.gridRowStart = `${parseInt(idx[0]) + 1}`;
-//         div.style.gridRowEnd = `span 1`;
-//         div.style.gridColumnStart = `${parseInt(idx[1]) + 1}`;
-//         div.style.gridColumnEnd = `span ${length}`;
-//         break;
-//       case "patrol":
-//         div.classList.add("patrol");
-//         div.style.gridRowStart = `${parseInt(idx[0]) + 1}`;
-//         div.style.gridRowEnd = `span 1`;
-//         div.style.gridColumnStart = `${parseInt(idx[1]) + 1}`;
-//         div.style.gridColumnEnd = `span ${length}`;
-//         break;
-//       default:
-//         return;
-//     }
-//   }
-
-//   return div;
-// }
-// // Random Button Logic
-// const shipArray = [
-//   {
-//     name: "carrier",
-//     length: 5,
-//     directions: ["horizontal", "vertical"],
-//   },
-//   {
-//     name: "battleship",
-//     length: 5,
-//     directions: ["horizontal", "vertical"],
-//   },
-//   {
-//     name: "destroyer",
-//     length: 4,
-//     directions: ["horizontal", "vertical"],
-//   },
-//   {
-//     name: "cruiser",
-//     length: 4,
-//     directions: ["horizontal", "vertical"],
-//   },
-//   {
-//     name: "submarine",
-//     length: 3,
-//     directions: ["horizontal", "vertical"],
-//   },
-//   {
-//     name: "patrol",
-//     length: 2,
-//     directions: ["horizontal", "vertical"],
-//   },
-// ];
-
-// const randomButton = document.querySelector(".randomize");
-// randomButton.addEventListener("click", (e) => {
-//   let randomNumber = generateRandomNumber(0, 99);
-//   let randomShip = createShipDom(randomNumber, "ship-type-carrier");
-//   shipsPlacedBoard.appendChild(randomShip);
-// });
-// function generateRandomNumber(max, min) {
-//   return Math.floor(Math.random() * (max - min) + min);
-// }
-// function generateRandomShipPositions(ship) {
-//   let randomDirection = generateRandomNumber(1, 0);
-//   let currentDirection = ship.directions[randomDirection];
-//   switch (direction) {
-//     case 0:
-//       direction = 1;
-//       break;
-//     case 1:
-//       direction = 10;
-//       break;
-//     default:
-//       return;
-//   }
-
-//   let randomStart = Math.abs(
-//     Math.floor(
-//       Math.random() * computerSquares.length -
-//         ship.directions[0].length * direction
-//     )
-//   );
-
-//   const isTaken = current.some((index) =>
-//     computerSquares[randomStart + index].classList.contains("taken")
-//   );
-//   const isAtRightEdge = current.some(
-//     (index) => (randomStart + index) % width === width - 1
-//   );
-//   const isAtLeftEdge = current.some(
-//     (index) => (randomStart + index) % width === 0
-//   );
-
-//   if (!isTaken && !isAtRightEdge && !isAtLeftEdge)
-//     current.forEach((index) =>
-//       computerSquares[randomStart + index].classList.add("taken", ship.name)
-//     );
-//   else generate(ship);
-// }
-/// generate board
 
 const generateBoard = (() => {
-  const p1Board = document.querySelector("#p1-board");
-  let p2Board = document.querySelector("#p2-board");
+  const p1Board = document.querySelector(".p1-board");
+  let p2Board = document.querySelector(".p2-board");
   for (let i = 0; i < 100; i++) {
     let cell1 = document.createElement("div");
     let cell2 = document.createElement("div");
 
-    cell1.classList.add("grid-cell");
+    cell1.classList.add("p1-grid-cell");
     cell1.dataset.id = i;
-    cell2.classList.add("grid-cell");
+    cell2.classList.add("p2-grid-cell");
     cell2.dataset.id = i;
     p1Board.appendChild(cell1);
     p2Board.appendChild(cell2);
   }
 })();
-// lets grab the corresponding ships divs
+
 const shipsContainer = document.querySelectorAll(".ship");
 // lets grab the board to be appended to
-const boardCells = document.querySelectorAll(".grid-cell");
+const p1BoardCells = document.querySelectorAll(".p1-grid-cell");
+console.log(p1BoardCells);
 const rotateBtn = document.querySelector(".rotate");
-const board = document.querySelector("#p1-board");
-
-// the objective is to append a selected ship into a grid cell
-// Id need to select the ship
-// id need to select the target grid cell
-// id need to set the set the head of my selected ship to the targeted grid cell
-// id need to be able to move it from cell to cell
+const p1Board = document.querySelector(".p1-board");
 
 // select a ship
 let activeShip = null;
@@ -289,8 +43,8 @@ shipsContainer.forEach((ship, index) => {
 });
 // set event listner on grids
 let valid = false;
-board.addEventListener("click", (e) => {
-  let cell = e.target.classList.contains("grid-cell");
+p1Board.addEventListener("click", (e) => {
+  let cell = e.target.classList.contains("p1-grid-cell");
   if (cell) {
     if (activeShip === null) return;
     if (!valid) return;
@@ -298,34 +52,14 @@ board.addEventListener("click", (e) => {
     placeShip(activeShip);
   }
   return;
-  let idx = parseInt(cell.getAttribute("data-id"));
-
-  // activeShip.body = generateShipBody(
-  //   idx,
-  //   activeShip.direction,
-  //   activeShip.length
-  // );
-
-  // console.log(activeShip.body);
-
-  // if (isOutsideBoundary(activeShip)) {
-  //   alert("outside boundry");
-  //   return;
-  // }
-  //
-  // if (isTaken(activeShip)) {
-  //   alert("is taken");
-  //   return;
-  // }
-
-  // console.log(cell);
 });
-
+// event
 rotateBtn.addEventListener("click", (e) => {
   if (activeShip == null) return;
   clearShip(activeShip);
   rotateShip();
 });
+//funtions
 function rotateShip() {
   activeShip.direction =
     activeShip.direction == "horizontal" ? "vertical" : "horizontal";
@@ -338,7 +72,7 @@ function switchActiveShip(index) {
   shipsContainer[index].classList.add("active");
 }
 function clearShip(ship) {
-  [...boardCells].filter((cell) => {
+  [...p1BoardCells].filter((cell) => {
     let result = cell.classList.contains(ship.name);
     if (result) {
       cell.classList.remove(ship.name);
@@ -348,10 +82,11 @@ function clearShip(ship) {
 }
 function placeShip(ship) {
   ship.body.forEach((cell) => {
-    boardCells[cell].classList.add(ship.name);
-    boardCells[cell].classList.add("taken");
+    p1BoardCells[cell].classList.add(ship.name);
+    p1BoardCells[cell].classList.add("taken");
   });
 }
+//to be replaced with the build body function in ship.js
 function generateShipBody(head, direction, length) {
   let body = [];
   let valid = false;
@@ -374,7 +109,7 @@ function isTaken(ship) {
   let result = false;
   for (let i = 0; i < ship.length; i++) {
     let cell = ship.body[i];
-    if (boardCells[cell].classList.contains("taken")) {
+    if (p1BoardCells[cell].classList.contains("taken")) {
       result = true;
       break;
     }
@@ -391,9 +126,7 @@ function isOutsideBoundary(ship) {
   }
   return result;
 }
-// function biggerThan99(coordinate) {
-//   return coordinate.some((element) => element > 99);
-// }
+
 function randomize(ship) {
   let randomDirection = Math.floor(Math.random() * 2);
   let randomSpot = Math.floor(Math.random() * 99);
@@ -427,7 +160,7 @@ randomBtn.addEventListener("click", (e) => {
 // if its valid if its not
 // do nothing
 // event lisner on grids
-boardCells.forEach((cell) => {
+p1BoardCells.forEach((cell) => {
   cell.addEventListener("mouseover", (e) => {
     if (activeShip === null) return;
     console.log(e.currentTarget);
@@ -453,17 +186,17 @@ boardCells.forEach((cell) => {
     valid = true;
   });
 });
-board.addEventListener("mouseleave", (e) => {
+p1Board.addEventListener("mouseleave", (e) => {
   clearHover();
 });
 
 function addHover(ship) {
   ship.body.forEach((cell) => {
-    boardCells[cell].classList.add("hover");
+    p1BoardCells[cell].classList.add("hover");
   });
 }
 function clearHover() {
-  [...boardCells].filter((cell) => {
+  [...p1BoardCells].filter((cell) => {
     let hover = cell.classList.contains("hover");
     let cursor = cell.style.cursor;
     if (hover) {
@@ -478,12 +211,13 @@ function clearHover() {
 const startBtn = document.querySelector(".start");
 startBtn.addEventListener("click", startGame);
 function startGame() {
-  const p1Board = document.querySelector("#p1-board");
-  const p2Board = document.querySelector("#p2-board");
+  const p1BoardContainer = document.querySelector(".p1-board-container");
+  const p2BoardContainer = document.querySelector(".p2-board-container");
   const shipSelectionContainer = document.querySelector(
     ".ship-selection-container"
   );
-  p1Board.classList.add("disable");
-  p2Board.classList.add("show");
+  p1BoardContainer.classList.add("disable");
+  p2BoardContainer.classList.add("show");
   shipSelectionContainer.classList.add("inactive");
+  activeShip = null;
 }
